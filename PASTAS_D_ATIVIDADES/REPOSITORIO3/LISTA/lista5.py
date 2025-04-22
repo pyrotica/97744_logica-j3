b4485601761562ae7d67d148c107cb391caebe6e
Miguel <perfectact.2023@gmail.com>
2025-04-22 19:35:33 +0000

    salvar

--- PASTAS_D_ATIVIDADES/REPOSITORIO3/LISTA/lista5.py
+++ PASTAS_D_ATIVIDADES/REPOSITORIO3/LISTA/lista5.py
@@ -2,24 +2,32 @@ import os
 os.system("clear")
 
 lista=[]
-QUANT=5
-for i in range(QUANT):
- num=int(input(f"digite a {1+i}º nota: "))
+QUANT=int(input("digite a quantidade de notas na lista: "))
+os.system("clear")
 
- lista.append(num)
+if QUANT>0:
+ for i in range(QUANT):
+  num=int(input(f"digite a {1+i}º nota: "))
+
+  lista.append(num)
 
 
-maior_l=max(lista)
-menor_l=min(lista)
-print()
-print("\t==========resultado==========\n")
-print(f"maior numero da lista: {maior_l}\n")
-print(f"menor numero da lista: {menor_l}\n")
-print(f"lista completa: {lista}\n")
-print("\t===LISTA===\n")
-for i, num in enumerate(lista, start=1):
- print(f"\n{i}º termo da lista: {num}")
+ maior_l=max(lista)
+ menor_l=min(lista)
+ print()
+ os.system("clear")
 
-print("\n\t===LISTA===\n")
-print("\t==========resultado==========")
+ print("\t==========resultado==========\n")
+ print(f"maior numero da lista: {maior_l}\n")
+ print(f"menor numero da lista: {menor_l}\n")
+ print(f"lista completa: {lista}\n")
+ print("\t===LISTA===\n")
+ for i, num in enumerate(lista, start=1):
+  print(f"\n{i}º termo da lista: {num}")
+
+ print("\n\t===LISTA===\n")
+ print("\t==========resultado==========")
+
+else:
+ print("Quantidade invalido")
  
