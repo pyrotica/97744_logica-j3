0fda625e41fb6f262d649084eea7e06794ad0354
Miguel <perfectact.2023@gmail.com>
2025-05-06 19:07:39 +0000

    salvar

---
+++ PASTAS_DE_ATIVIDADES_M/TestesAleatorios.png/testo_n2.py
@@ -0,0 +1,22 @@
+import os
+import random 
+import time
+os.system("clear")
+
+alea_num=random.randint(1, 10)
+num= int(input("digite um numero entre 1 a 10: ")) 
+
+if alea_num==num:
+    print("VOCÊ VENCEU")
+
+else:
+    while True:
+        time.sleep(0.1)
+        print("""
+
+    ╔════•ೋೋ•════╗              
+        perdedor
+    ╚════•ೋೋ•════╝
+
+         """)
+    
